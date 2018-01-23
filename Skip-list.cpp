f06bdadf63c15f7192e69ea2b5bd5c4fd8178c18
zuoguangsheng <zuoguangsheng@coding.net>
2018-01-23 07:40:34 -0800

    fix

--- Skip-list.cpp
+++ Skip-list.cpp
@@ -6,43 +6,50 @@
 #define P 4
 
 struct node{
-    int value;
-    node** forward;
-    int info; 
-    int maxlevel; 
-    node(int v = 0) {
-    	value = v;
-      maxlevel = 1;
-    	forward = new node* [MAXLEVEL]; 
-    }
+  int value;
+  node** forward;
+  int info; 
+  int maxlevel; 
+  node(int v = 0, int ml=1, int i=0) {
+    value = v;
+    maxlevel = ml;
+    info = i;
+    forward = new node* [MAXLEVEL]; 
+  }
 };
 
 class SkipList{
 
 public:
-	SkipList() {
-		list = new node;
-		srand(time(NULL)); 
-	}
-	node* insert (int v);
-	bool del (int v);
-	node* find (int v);
-
+  SkipList() {
+    list = new node(0,MAXLEVEL); // list headers must be init with maxlevel 
+    srand(time(NULL)); 
+  }
+  node* insert (int v);
+  bool del (int v);
+  node* find (int v);
+  void display(int level) {
+    for (node* iterator = list; iterator->forward[level-1] != NULL; 
+		    iterator = iterator->forward[level-1])
+	printf("%d:%d ",iterator->forward[level-1]->value, iterator->forward[level-1]->info);
+    puts("");
+   
+  }
 private:
   node* list;
-	int random_level(){
-		int l = 1;
-		while (l < MAXLEVEL - 1 && rand() % P + 1 <= 1) 
-			l ++;
-		return l;
-	}
+  int random_level(){
+    int l = 1;
+    while (l < MAXLEVEL && rand() % P + 1 <= 1) 
+      l ++;
+    return l;
+  }
 };
 
 node* SkipList::insert(int v) {
   node* update[MAXLEVEL];
   node* iterator = list;
-  puts("done1");
-	for (int level = list->maxlevel - 1 ; level >= 0; --level) {
+  for (int level = 0; level < MAXLEVEL; ++level) update[level] = NULL;
+  for (int level = list->maxlevel - 1 ; level >= 0; --level) {
     while (iterator->forward[level] != NULL && iterator->forward[level]->value < v)
       iterator = iterator->forward[level];
     // if the v is already in the skip list , then change the info attr.
@@ -50,29 +57,27 @@ node* SkipList::insert(int v) {
       iterator->forward[level]->info += 1;
       return iterator->forward[level];
     } else {
-      update[level] = iterator->forward[level];
+      update[level] = iterator;
     }
   }
-  puts("done2");
   int v_level = random_level();
-  node* v_node = new node(v);
-  for (int i = 0; i < v_level; ++i) {
-    v_node->forward[i] = update[i];
-    update[i] = v_node->forward[i];
+  node* v_node = new node(v,v_level,1);
+  for (int i = 0; i < v_level; ++i) { 
+    v_node->forward[i] = update[i]->forward[i];
+    update[i]->forward[i] = v_node;
   }
-  puts("done3");
 }
 
 node* SkipList::find(int v) {
   node * iterator = list;
-  for (int level = list->maxlevel - 1; level >= 0; --level) {
+  for (int level = list->maxlevel - 1; level >= 0; --level) { 
     while (iterator->forward[level] != NULL && iterator->forward[level]->value < v)
       iterator = iterator->forward[level];
     if (iterator->forward[level] != NULL && iterator->forward[level]->value == v) {
       return iterator->forward[level];
     }
-    return NULL;
   }
+  return NULL;
 }
 
 
--- test.cpp
+++ test.cpp
@@ -8,18 +8,23 @@ int main(){
 	SkipList* slist = new SkipList;
 	int n,m,x,y,c;
 	scanf("%d %d",&n, &m);
-	printf("%d %d",n,m);
+	printf("%d %d\n",n,m);
 	for (int i = 1; i <= n; ++i) {
 		scanf("%d",&x);
 		slist->insert(x);
-		printf("%d\n",i);
 	}
+	slist->display(1);
+	int cc = 0;
 	for (int i = 1; i <= m; ++i) {
 		scanf("%d%d",&y,&c);
 		node* p = slist->find(y);
-		assert(y==p->value);
-		assert(c==p->info);
+		if (!p){
+	       	  printf("%d %d\n",y,c);
+		  if (c!=0) ++cc;
+		}
+	       else printf("%d=?%d %d=?%d\n",y,p->value,c,p->info);
 	}
+	printf("%d\n",cc);
 	puts("done");
 	return 0;
 }
